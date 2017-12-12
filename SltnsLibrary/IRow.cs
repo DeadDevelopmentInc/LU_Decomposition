f3cf022e18f88b1854054419c4f6b17298f3583c
Dead Development Inc <demidov15alexsand@gmail.com>
2017-12-12 17:36:45 +0300

    Add class and their realization.

---
+++ SltnsLibrary/IRow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SltnsLibrary
+{
+    public interface IRow
+    {
+        // записывает значение value в ячейку с индексом num
+        void setValue(int num, double value);
+
+        // добавляет value к существующему значению в ячейке num
+        void addValue(int num, double value);
+
+        // возвращает значение ячейки num
+        double getValue(int num);
+
+        // возвращает все ненулевые ячейки строки/столбца: 
+        // индексы ячеек - в indexes, значения в values
+        void getValues(ref int[] indexes, ref double[] values);
+    }
+}
